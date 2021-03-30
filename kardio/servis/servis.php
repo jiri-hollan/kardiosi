e6dccdede428d4773b718467e8c2f02f6144d82c
Jiri Hollan <jiri.hollan@gmail.com>
2021-03-30 20:10:39 +0200

    servis: nalaganje mes.pdf iz http povezave

---
+++ kardio/servis/servis.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <!-- Always force latest IE rendering engine or request Chrome Frame -->
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+<body>
+ <section class="top-bar-section">
+          <!-- Right Nav Section -->
+          <ul class="right">
+              <li class=""><a href="upload/formUpload.html"><h1>naloži mesečni razpis PDF</h1></a></li>
+              
+          </ul>
+        </section>
+</body>
+</html>
