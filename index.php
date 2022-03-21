e1ba1c60b82b1f30afcbe5b6a2071c776f60fc92
Jiří Hollan <jiri.hollan@gmail.com>
2022-03-21 14:36:32 +0100

    dodan index iz weba

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+
+
+
+?>
+
+<!DOCTYPE html
+PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+<head>
+  <meta name="generator" content="HTML Tidy for Windows (vers 13 April 2006), see www.w3.org" />
+  <meta http-equiv="Content-Type" content="text/html;charset=utf-8_general_ci" />
+
+  <meta name="keywords" content="kardio,hollan" />
+  <meta http-equiv="refresh" content="0;url=kardio/index.php" />
+
+  <title>Anestezija za operacije srca</title>
+</head>
+
+
+    <body>
+    </body>
+  
+</html>
