95bd9ef894eb3f94d8cac8443844b6a9f1657e22
Jiri Hollan <jiri.hollan@gmail.com>
2022-03-21 18:19:04 +0100

    ODS prekodirane na xlsx

--- kardio/js/razpis.js
+++ kardio/js/razpis.js
@@ -17,7 +17,7 @@ function arhivRazpis() {
   var x=""
   x=(document.getElementById("letnikx").value);
 if (x!="") {
- window.location="razpis/msBackup/arhiv/" + x +".ods";//zdaj je window.location="skrita1.htm"
+ window.location="razpis/msBackup/arhiv/" + x +".xlsx";//zdaj je window.location="skrita1.htm"
   } else {
   alert("Vpisi iskano letnico");
 }
