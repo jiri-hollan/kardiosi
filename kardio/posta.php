3df8e8719b0b28695bc13d0fc6daf29e7112672d
Jiri Hollan <jiri.hollan@gmail.com>
2022-03-23 16:16:08 +0100

    Igranje z datoteko post

--- kardio/posta.php
+++ kardio/posta.php
@@ -1,4 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+</head>
+<body>
+
 <?php
+echo 'na začetku';
 $to = "jiri.hollan@gmail.com";
 $subject = "My subject";
 $txt = "Hello world!";
@@ -6,4 +14,8 @@ $headers = "From: webmaster@example.com" . "\r\n" .
 "CC: jiri.hollan@gmail.com";
 
 mail($to,$subject,$txt,$headers);
+echo "kao da je poslano";
 ?>
+
+</body>
+</html>
