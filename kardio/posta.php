715805775322a13e90442a8b6e5253388dcbc557
Jiri Hollan <jiri.hollan@gmail.com>
2022-03-25 16:43:15 +0100

    $headrs direktno iz array

--- kardio/posta.php
+++ kardio/posta.php
@@ -8,17 +8,19 @@
 <?php 
 echo 'na začetku<br>'; 
 $to = "jiri.hollan@gmail.com"; 
-$subject = "My subject"; 
+$subject = "My subject, posta.php"; 
 $txt = "Hello world!"; 
 
 //$headers = "From: webmaster@example.com" . "\r\n" . 
 //"CB: hocimin68@gmail.com"; 
- $headers[] = $to;
- $headers[] = "From: webmaster@example.com";
- 
-//if (mail($to,$subject,$txt,$headers)) { 
-  if  (mail($to,$subject,$txt,implode("\r\n", $headers))){
 
+ $headers[] = "From: webmaster@example.com";
+ $headers[] = $to;
+ $headers[] = "X-Mailer: PHP". phpversion();
+//if (mail($to,$subject,$txt,$headers)) { 
+ // if  (mail($to,$subject,$txt,implode("\r\n", $headers))){
+	 
+  if  (mail($to,$subject,$txt,$headers)){
 echo "<br>izgleda, da je poslano"; 
 } else {
 echo "<br>pošiljanje ni uspelo"; 
