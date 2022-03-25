cc6637de90725a1dbf9ad85a1fdc0380ffecda9f
Jiri Hollan <jiri.hollan@gmail.com>
2022-03-25 21:07:17 +0100

    dodanCc

--- kardio/postax.php
+++ kardio/postax.php
@@ -10,9 +10,10 @@
 $to = "<jiri.hollan@gmail.com>";
 $subject = "To je subjekt";
 $from = '<noreply@sender.com>';
-$message = "Hello world!";
+$message = "Hello world!Arrays";
 $headers[] = "From: " .($from);
 $headers[] = $to;
+$headers[] = "CC: somebodyelse@example.com";
 $headers[] = "Reply-To: ".($from);
 $headers[] = "Return-Path: ".($from);
 $headers[] = "MIME-Version: 1.0"; 
