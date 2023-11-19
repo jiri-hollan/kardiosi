b47e3c704f726780034f174b86b99a85e7b0f177
Jiri Hollan <jiri.hollan@gmail.com>
2023-11-19 12:45:59 +0100

    dodano par fajlov. stežni ni sprogramiran

---
+++ kardio/admin/kreateTableVse.php
@@ -0,0 +1,65 @@
+
+<?php
+require_once '../skupne/sabloni/zahlavi.php';
+
+echo '
+<h2>Naredi tabelo</h2>
+<form method="post" action="'. htmlspecialchars($_SERVER["PHP_SELF"]).'">  
+
+
+
+  <label for="uporabniki"><span class="imenaFilov">uporabniki</span></label>
+  <input type="radio" id="uporabniki" name="name" value="uporabniki">
+  
+ 
+   
+   <input type="hidden" name="nazaj" value='. $nazaj.'>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+  <br>
+</form>';
+?>
+<?php
+// define variables and set to empty values
+$name  = "";
+$ime  = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST"&& isset($_POST["name"])) {
+  $name = test_input($_POST["name"]);    
+  $name = naredi($name);
+
+}
+
+function naredi($ime) {
+require_once '../skupne/narediTablo.php';	
+$databaseGloboka=new DatabaseGloboka;	
+switch ($ime) {	
+
+
+case "uporabniki":
+$definice= "id INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	`email` varchar(255) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,
+	`uname` varchar(255) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,
+	`geslo` varchar(255) CHARACTER SET utf8 COLLATE utf8_slovenian_ci NOT NULL,
+	`ime` varchar(255) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,
+	`priimek` varchar(255) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,
+    `status` int(3) NOT NULL,
+    `pristop` int(3) NOT NULL,	
+	UNIQUE (email, uname)";
+$databaseGloboka->naredi('uporabnikiTbl2', $definice);
+break;
+
+/*
+case "":
+$definice= "";
+$databaseGloboka->naredi('', $definice);
+break;
+*/
+default:
+    echo "ni izvelo get case"; 
+  }//od switch	$ime
+ echo "<h2>Your Input: ".$ime."</h2>";
+echo "<br>"; 
+}// od function naredi($ime)
+
+require_once '../skupne/sabloni/zapati.php';
+?>
