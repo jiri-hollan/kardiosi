a350f60c030fad0bb17e138f3204895a2a977ec3
Jiri Hollan <jiri.hollan@gmail.com>
2023-11-20 18:26:55 +0100

    naredilo je bazo

--- kardio/admin/databazeMenu.php
+++ kardio/admin/databazeMenu.php
@@ -1,15 +1,17 @@
 <?php
 echo 'napredna manipulacija z bazo';
-require_once('../admin/sabloni/vkladane/zahlavi.php');
-require_once('../admin/administrace.php');
+require_once('sabloni/zahlavi.php');
+/*
+require_once('administrace.php');
 
 class Vertikal extends Administrace {
    public function __construct() {
 	       parent::__construct();
 		   
- /* if (isset($_SESSION["status"]) && $_SESSION["status"] == 3)  {*/
-$nazaj="../admin1/vertikalMenu.php";
+ /* if (isset($_SESSION["status"]) && $_SESSION["status"] == 3)  {
+$nazaj="databazeMenu.php";
 //-----------------nova koda-------------------
+*/
 ?>
 <form>
 <input type="hidden" class="" name="nazaj" value="$mazaj">
@@ -23,15 +25,16 @@ echo '
 <h1>Menu databaze</h1>
 <ul id="linky1">
 
-<li><a href="kreateBaseNavodila.php?nazaj='.$nazaj.'">naredi bazo: navodila</a></li>
+<li><a href="kreateBaseBiznis.php?nazaj='.$nazaj.'">naredi bazo: biznis</a></li>
 <li><a href="kreateTableVse.php?nazaj='.$nazaj.'">naredi tabele</a></li>
 </ul>
 ';
   /*   } else {
 	echo	' <h2>za ta del niste pooblaščeni</h2>';
-	}*/
+
    }//od construct 
 }//od class vertikal  
  $adminVnertikal = new Vertikal(); 
-require_once('../admin/sabloni/vkladane/zapati.php'); 
+ 	}*/
+require_once('sabloni/zapati.php'); 
 ?>
