69cc708af98abe2fa67dbbe1e633bb880872f1c4
Jiri Hollan <jiri.hollan@gmail.com>
2023-12-01 07:52:49 +0100

    izbris obsolentnega

--- kardio/index.php
+++ kardio/index.php
@@ -11,7 +11,7 @@ header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </meta> 
   <link rel="stylesheet" href="css/index.css?<?php echo time(); ?>">
-  <link rel="stylesheet" type="text/css"  href="css/style.css?<?php echo time(); ?>" >
+  <link rel="stylesheet" href="css/style.css?<?php echo time(); ?>" >
 <title>prikaz mesečnega razporeda v obliki PDF</title>
 <?php
 $direktorij = "";
