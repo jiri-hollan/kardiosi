41bd3b983351d19fcd2e97d514c9ecf04e4c83fc
Jiri Hollan <jiri.hollan@gmail.com>
2023-12-02 21:29:52 +0100

    dodan link

--- kardio/admin/databazeMenu.php
+++ kardio/admin/databazeMenu.php
@@ -36,6 +36,7 @@ echo'
 <h1>Menu servis</h1>
 <ul id="linky1">
 
+<li><a href="manipulaceObjektUniverzal.php?nazaj='.$nazaj.'">uporabniki univerzal</a></li>
 <li><a href="manipulacePregledovalci.php?nazaj='.$nazaj.'">uporabniki</a></li>
 
 
