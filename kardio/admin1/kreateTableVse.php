b76093c7bc24a2312a6e6f0cf119eb3b128cef51
Jiri Hollan <jiri.hollan@gmail.com>
2023-12-03 20:49:01 +0100

    izbris nepotrebnih

--- kardio/admin1/kreateTableVse.php
+++ kardio/admin1/kreateTableVse.php
@@ -71,25 +71,7 @@ $definice= "id INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 $databaseGloboka->naredi('uporabnikiTbl', $definice);
 break;
 
-case "limiti":
-$definice= "id INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	`bolnisnica` varchar(255) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,	
-	`skupina` varchar(255) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,	
-	`ime` varchar(255) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,
-    `min` float(4) NOT NULL,
-	`max` float(4) NOT NULL";
-$databaseGloboka->naredi('limitiTbl', $definice);
-break;
-/*.............................................................*/
 
-case "ocena":
-$definice= "id INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	`bolnisnica` varchar(255) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,		
-	`ime` varchar(255) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,
-	`ocena` varchar(255) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,	
-    `status` int(3) NOT NULL";
-$databaseGloboka->naredi('ocenaTbl', $definice);
-break;
 
 /*...............................................................*/
 case "pregledovalci":
@@ -101,74 +83,6 @@ $definice= "id INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 $databaseGloboka->naredi('pregledovalciTbl', $definice);
 break;
 
-case "bolnisnice":
-$definice= "id INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	mesto varchar(255) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,
-	nazivB varchar(255) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,
-    status int(3) NOT NULL,
-    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP";
-$databaseGloboka->naredi('bolnisniceTbl', $definice);
-break;
-
-case "sklepi":
-$definice= "id INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	`bolnisnica` varchar(255) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,	
-	`sklep` varchar(255) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,
-    `status` int(3) NOT NULL";
-$databaseGloboka->naredi('sklepiTbl', $definice);
-break;
-
-case "bolnik":
-$definice= "pregledId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-datPregleda DATE,
-imeZdravnika VARCHAR(100),
-stevMaticna INTEGER,
-ustanova VARCHAR(30),
-EMSO NUMERIC(20), 
-dan INTEGER,
-mesec INTEGER,
-leto INTEGER,
-datRojstva VARCHAR(100),
-starost NUMERIC(3),
-ime VARCHAR(100),
-priimek VARCHAR(100),
-oddelek VARCHAR(100),
-dgOperativna VARCHAR(100),
-opNacrtovana VARCHAR(100),
-teza DECIMAL(5),
-visina DECIMAL(4,2),
-bmi INTEGER,
-krvniTlak VARCHAR(100),
-pulz NUMERIC(3),
-spo2 NUMERIC(3),
-hb DECIMAL(3),
-ks DECIMAL(3,1),
-inr DECIMAL(3,1),
-aptc DECIMAL(3),
-trombociti DECIMAL(4),
-kreatinin DECIMAL(3),
-laktat NUMERIC(2,1),
-pbnp NUMERIC(3),
-pct NUMERIC(3,1),
-crp NUMERIC(3),
-na NUMERIC(3),
-k NUMERIC(2,1),
-drugiIzvidi VARCHAR(100),
-ekg VARCHAR(255),
-rtg VARCHAR(255),
-dgPridruzene VARCHAR(255),
-terPredhodna VARCHAR(255),
-asa INTEGER,
-mallampati INTEGER,
-alergija VARCHAR(100),
-izvidiInOpombe BLOB(2147483647),
-premedVecer VARCHAR(100),
-premedPredOp VARCHAR(100),
-navodila VARCHAR(255),
-sklep VARCHAR(255),
-status CHARACTER(15)";
-$databaseGloboka->naredi('bolnikTbl', $definice);
-break;
 
 /*
 case "":
