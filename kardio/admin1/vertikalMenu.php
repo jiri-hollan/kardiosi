b31481ccab3bdec68127631386b4f988256a4e1f
Jiri Hollan <jiri.hollan@gmail.com>
2023-12-03 20:46:12 +0100

    namesto uporabnikiTbl2 le uporabnikiTbl

--- kardio/admin1/vertikalMenu.php
+++ kardio/admin1/vertikalMenu.php
@@ -7,7 +7,7 @@ class Vertikal extends Administrace {
    public function __construct() {
 	       parent::__construct();
 		   
-  if (isset($_SESSION["status"]) && $_SESSION["status"] == 3)  {
+ /* if (isset($_SESSION["status"]) && $_SESSION["status"] == 3)  {*/
 $nazaj="../admin1/vertikalMenu.php";	  
 echo '
 
@@ -29,9 +29,9 @@ echo '
 <li><a href="kreateTableVse.php?nazaj='.$nazaj.'">naredi tabele</a></li>
 </ul>
 ';
-     } else {
+  /*   } else {
 	echo	' <h2>za ta del niste pooblaščeni</h2>';
-	}
+	}*/
    }//od construct 
 }//od class vertikal  
  $adminVnertikal = new Vertikal(); 
