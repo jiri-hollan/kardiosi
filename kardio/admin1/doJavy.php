0f3735ea33f0dba346fb28657a8d3407528561a7
Jiri Hollan <jiri.hollan@gmail.com>
2023-12-06 13:36:04 +0100

    v dojavy pokaže šumnike

--- kardio/admin1/doJavy.php
+++ kardio/admin1/doJavy.php
@@ -3,21 +3,23 @@
 <body>
 <?php
 $php_var1 ="Hello world";
+echo $php_var1;
 $php_var2 ="Jiří";
-echo json_encode($php_var1);
-echo '<br>'.json_encode($php_var2);
+echo $php_var2;
+echo json_encode($php_var1, JSON_UNESCAPED_UNICODE);
+echo '<br>'.json_encode($php_var2, JSON_UNESCAPED_UNICODE);
 
 echo '<script>';
-echo 'var js_variable1= ' . json_encode($php_var1) . ';';
-echo 'var js_variable2= ' . json_encode($php_var2) . ';';
+echo 'var js_variable1= ' . json_encode($php_var1, JSON_UNESCAPED_UNICODE) . ';';
+echo 'var js_variable2= ' . json_encode($php_var2, JSON_UNESCAPED_UNICODE) . ';';
 echo '</script>';
 ?>
 <p> naj bi se pokazalo okno alert</p>
 
 <script>
 //alert("poskus");
-alert("variabla 1:" + js_variable1);
-alert("variabla 2:" + js_variable2);
+alert("variabla a1:" + js_variable1);
+alert("variabla a2:" + js_variable2);
 </script>
 </body>
 </html>
