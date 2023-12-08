7c10aeec3013635b7fda416a739fefd40c4ff4a8
Jiri Hollan <jiri.hollan@gmail.com>
2023-12-08 17:58:33 +0100

    Nov fajl

---
+++ kardio/sortaPoskus.php
@@ -0,0 +1,78 @@
+<!--<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style type="text/css">
+body {background-color: wheat}
+a {
+color: black
+}
+a {
+text-decoration: none
+}
+a.zdrav:hover {font-size: 130%;background-color:maroon; color:white}
+a.okro:hover {font-size: 130%;background-color:blue; color:yellow}
+
+
+</style>-->
+<?php
+require_once('frontend/sabloni/zahlavi.php');
+?>
+
+<script src="js/clani.js?<?php echo time(); ?>" type="text/javascript">
+  </script>
+</head>
+<body id="sorta">
+
+<script>
+var skupina = "ČLANI";
+var b ="";
+b += "<p>";
+b += "<a class='zdrav' ";
+b += "href='javascript: showZDR(";
+b += '"servis/poskusArrayZdravniki.php?pogoj=1")';
+b += "' > ";
+b += skupina;
+b += "</a>";
+b +=  "</p>";
+document.write(b);
+
+var skupina = "PRIDRUŽENI";
+var b ="";
+b += "<p>";
+b += "<a class='zdrav' ";
+b += "href='javascript: showZDR(";
+b += '"servis/poskusArrayZdravniki.php?pogoj=2")';
+b += "' > ";
+b += skupina;
+b += "</a>";
+b +=  "</p>";
+document.write(b);
+
+
+
+</script>
+
+<b id= "okr"> to je mail </b>
+
+<SCRIPT>
+var c = "";
+var p = ""
+var r = ""
+for (i=0;i<zdrClan.length;i++)
+{
+if (zdrClan[i].rang==1 || zdrClan[i].rang==2) 
+{
+p += zdrClan[i].email + ","
+}
+r = "<a  class='okro' href='mailto:" + p + "'" + "target='_top'>" + "okro&#382;nica" + "</a>" 
+}
+
+document.getElementById('okr').innerHTML = "<br>" + r;
+</script>
+</body>
+</html>
