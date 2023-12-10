6f874a8c13c3f80f41aa8da98a556711b738745c
Jiri Hollan <jiri.hollan@gmail.com>
2023-12-10 16:23:29 +0100

    odstranjen link na pregledovalce

--- kardio/admin/databazeMenu.php
+++ kardio/admin/databazeMenu.php
@@ -36,7 +36,6 @@ echo'
 <h1>Menu servis</h1>
 <ul id="linky1">
 
-<li><a href="manipulaceObjektUniverzal.php?tabulka=pregledovalciTbl">upravljanje z pregledovalci</a></li>
 <li><a href="manipulaceObjektUniverzal.php?tabulka=uporabnikiTbl">upravljanje z uporabniki</a></li>
 <li><a href="../admin1/vertikalMenu.php?nazaj='.$nazaj.'">Vertikal Menu</a></li>
 <li><a href="../servis/manipulacePogojUniverzal.php?tabulka=uporabnikiTbl">Poskus pogoj</a></li>
