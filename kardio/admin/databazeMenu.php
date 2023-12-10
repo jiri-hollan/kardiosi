bd1e0b5197e61ac842fc465aafaa266f032038de
Jiri Hollan <jiri.hollan@gmail.com>
2023-12-10 18:58:48 +0100

    Izbris zaixanih delov kode
    mislim,da je bilo nekaj zaixanega pomotoma

--- kardio/admin/databazeMenu.php
+++ kardio/admin/databazeMenu.php
@@ -2,30 +2,9 @@
 require_once('../skupne/sabloni/zahlavi.php');
 echo 'napredna manipulacija z bazo';
 
-/*
-require_once('administrace.php');
-
-class Vertikal extends Administrace {
-   public function __construct() {
-	       parent::__construct();
-		   
- /* if (isset($_SESSION["status"]) && $_SESSION["status"] == 3)  {
-$nazaj="databazeMenu.php";
-//-----------------nova koda-------------------
-*/
-?>
-<!--<form>
-<input type="hidden" class="" name="nazaj" value="$nazaj">
-<input type="submit" name="" value="">
-</form>-->
-<?php
-//-------------------------------------------------------
 echo '
-
-
 <h1>Menu databaze</h1>
 <ul id="linky1">
-
 <li><a href="kreateBaseBiznis.php?nazaj='.$nazaj.'">naredi bazo: biznis</a></li>
 <li><a href="kreateTableVse.php?nazaj='.$nazaj.'">naredi tabele</a></li>
 </ul>
@@ -41,12 +20,6 @@ echo'
 </ul>
 </div>
 ';
-  /*   } else {
-	echo	' <h2>za ta del niste pooblaščeni</h2>';
 
-   }//od construct 
-}//od class vertikal  
- $adminVnertikal = new Vertikal(); 
- 	}*/
 require_once('sabloni/zapati.php'); 
 ?>
