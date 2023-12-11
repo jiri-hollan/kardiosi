a7632b67b2d99b07e6a4fd7973772da35dc49b7c
Jiří Hollan <jiri.hollan@gmail.com>
2023-12-11 15:00:37 +0100

    dodan indexn na preusmeritev na fronend

---
+++ kardio/index.php
@@ -0,0 +1,10 @@
+<?php
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+?>
+<html> 
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8_general_ci" />
+<meta http-equiv="refresh" content="0;url=frontend/menuMain.php" />
+</head>
+</html>
