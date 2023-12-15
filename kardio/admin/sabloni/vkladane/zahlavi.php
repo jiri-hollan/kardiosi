01237ce4e87dcc353246cef80da915c59492924f
Jiří Hollan <jiri.hollan@gmail.com>
2023-12-15 14:45:24 +0100

    vkladane v sabloni in napolnjeno

---
+++ kardio/admin/sabloni/vkladane/zahlavi.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="cs-SI">
+<head>
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Admin</title>
+<link rel="shortcut icon" href="../favicon.ico?<?php echo time(); ?>">
+<link rel="stylesheet" href="../admin/css/zahlavi.css?<?php echo time(); ?>">
+<link rel="stylesheet" href="../admin/css/menuFile.css?<?php echo time(); ?>">
+<link rel="stylesheet" href="../admin/css/izbira.css?<?php echo time(); ?>">
+</head>
+<body>
+<div class="topnav">
+  <a class="active" href="../index.php" target="_top">Domov</a>
+  <!--<a href="../frontend/prihlaseni.php?r=logout&stav=odhlasit">Odjava in prijava</a>-->
+</div>
+ <?php 
+ //require_once('../admin/sabloni/oznamovaci-oblast.php');  
+ function test_input($test) {
+  $test = trim($test);
+  $test = stripslashes($test);
+  $test = htmlspecialchars($test);
+  return $test;
+} 
+ ?> 
+
+
