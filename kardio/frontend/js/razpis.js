a03f43179b1717e90c376826b196510249f22b24
Jiří Hollan <jiri.hollan@gmail.com>
2023-12-18 10:22:17 +0100

    dodani novi folderji in napolnjeni

---
+++ kardio/frontend/js/razpis.js
@@ -0,0 +1,30 @@
+//var x=""
+//var y=""
+function aktualRazpis(){
+  var x=""
+  x=(document.getElementById("letnikx").value);
+if (x!=""){
+ window.location="../razpis/msofice/" + x +".xlsx";//zdaj je window.location="skrita1.php"
+  } else {
+ alert("Vpisi iskano letnico ");
+  }
+}//od function aktualRazpis
+
+
+//var x=""
+//var y=""
+function arhivRazpis() {
+  var x=""
+  x=(document.getElementById("letnikx").value);
+if (x=="") {
+   alert("Vpisi iskano letnico");
+  } else {
+	  oo="../razpis/msBackup/arhiv/" + x +".ods";
+	  ms="../razpis/msBackup/arhiv/" + x +".xlsx";
+	  window.location=ms;
+  
+	  
+	  //zdaj je window.location="skrita1.php"
+
+}
+} //od function arhivRazpis
