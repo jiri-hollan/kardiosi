c6486e68a3de7a031a6d3cced5c2ed4be642152b
Jiří Hollan <jiri.hollan@gmail.com>
2023-12-18 10:33:50 +0100

    spremenjeni fajli

--- kardio/servis/servis.php
+++ kardio/servis/servis.php
@@ -1,18 +1,16 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <!-- Always force latest IE rendering engine or request Chrome Frame -->
-    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-</head>
-<body>
+<?php
+require_once '../skupne/sabloni/zahlavi.php';
+$nazaj="../servis/servis.php";
+
+
+echo'
  <section class="top-bar-section">
           <!-- Right Nav Section -->
           <ul class="right">
               <li class=""><a href="upload/formUpload.html"><h1>naloži mesečni razpis PDF</h1></a></li>
-              
+              <li class=""><a href="manipulaceUporabniki.php?nazaj='.$nazaj.'">kompetence</a></li> 
           </ul>
         </section>
-</body>
-</html>
+';
+?>
+
