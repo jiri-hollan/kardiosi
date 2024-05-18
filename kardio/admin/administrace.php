3d0209769cd0865200a7f1bb6fadc669d6056993
Jiri Hollan <jiri.hollan@gmail.com>
2024-05-18 15:11:52 +0200

    popravilo datotek prekopiranih iz anestiz

--- kardio/admin/administrace.php
+++ kardio/admin/administrace.php
@@ -10,9 +10,9 @@ class Administrace {
 	  $this->conn = new Database();
       $this->zaklad = new stdClass();
 	  if ($_SERVER['SERVER_NAME']=="localhost"){
-		 $this->zaklad->url = 'http://' . $_SERVER['SERVER_NAME'].'/kardiosi/admin/'; 
+		 $this->zaklad->url = 'http://' . $_SERVER['SERVER_NAME'].'/kardiosi/kardio/admin/'; 
 	  }else {
-		 $this->zaklad->url = 'http://' . $_SERVER['SERVER_NAME'].'/admin/';  
+		 $this->zaklad->url = 'http://' . $_SERVER['SERVER_NAME'].'/kardio/admin/';  
 	  }
 	/*  $casoviLimit = 600;
 	  if (isset($_SESSION["uporabnikPrihlasen"])) {
--- kardio/skupne/administrace.php
+++ kardio/skupne/administrace.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once('../skupne/database.php');
-require_once('sabloni/vkladane/zahlavi.php');
+
 class Administrace {
 	public $conn;
 	public $zaklad;
@@ -10,20 +10,17 @@ class Administrace {
 	  $this->conn = new Database();
       $this->zaklad = new stdClass();
 	  if ($_SERVER['SERVER_NAME']=="localhost"){
-		 $this->zaklad->url = 'http://' . $_SERVER['SERVER_NAME'].'/anestiz/frontend/'; 
+		 $this->zaklad->url = 'http://' . $_SERVER['SERVER_NAME'].'/kardiosi/kardio/admin/'; 
 	  }else {
-		 $this->zaklad->url = 'http://' . $_SERVER['SERVER_NAME'].'/frontend/';  
+		 $this->zaklad->url = 'http://' . $_SERVER['SERVER_NAME'].'/kardio/admin/';  
 	  }
-		  
-      //$this->zaklad->url = 'http://' . $_SERVER['SERVER_NAME'].'/anestiz/frontend/';
-	  //echo $this->zaklad->url;
 	  $casoviLimit = 600;
 	  if (isset($_SESSION["uporabnikPrihlasen"])) {
 		  $uplinuliCas = time() - $_SESSION["casova_znamka"];
 		  if ($uplinuliCas > $casoviLimit) {
 			  session_unset();
 			  session_destroy();
-			  header('Location: ' . $this->zaklad->url . 'prihlaseni.php?stav=neaktivni');
+			  header('Location: ' . $this->zaklad->url . '../frontend/prihlaseni.php?stav=neaktivni');
 			  exit();
 		  }
 	  }
@@ -32,20 +29,10 @@ class Administrace {
 	  if (empty($prihlasen)) {
 		  session_unset();
 		  session_destroy();
-	echo'<script>
-	sessionStorage.removeItem("testJSON");	
-	sessionStorage.removeItem("bolnikId"); 
-	</script>';	  
-		  
-		  header('Location: ' . $this->zaklad->url . 'prihlaseni.php?stav=odhlasit');
-		   
-	// header('Location: localhost/anestiz/frontend/prihlaseni.php?stav=odhlasit');
-		   
-		   exit();
+		  header('Location: ' . $this->zaklad->url . '../frontend/prihlaseni.php?stav=odhlasit');		   
+		  exit();
 	  } else {
 		  $this->conn = new Database();
-	  }
-//od construct	  
-	}
-//0d class administrace	
+	  }//od construct	  
+	}//0d class administrace	
 }
