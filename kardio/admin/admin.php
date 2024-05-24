13355102c716329f52238b731d0e279b6148f23d
Jiri Hollan <jiri.hollan@gmail.com>
2024-05-24 16:50:45 +0200

    admin.php preusmeri na databazeMenu.php

--- kardio/admin/admin.php
+++ kardio/admin/admin.php
@@ -5,4 +5,57 @@ header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 require_once('sabloni/vkladane/zahlavi.php');
 require_once('administrace.php');
 
+
+
+
+
+class MenuBiznis  {
+   public function __construct() { 
+ $a0= '
+
+';
+$a1= '
+
+';
+$a2= '
+
+'; 
+$a3= '
+../admin/databazeMenu.php
+';  
+echo '
+<nav id= "glavnaNav">
+<ul id="links">';
+  if (isset($_SESSION["status"]))  {
+	  //require_once('../skupne/menu-items.php'); 
+	   switch ($_SESSION["status"]) {		   
+	case 1:
+	  
+    break;   
+     case 2:
+	  
+	 break;	 
+	 case 3:
+	  header("Location: ../admin/databazeMenu.php");
+	   echo $a3;
+    break;   
+    default:
+	   } //od switch
+	 echo
+	'<script>
+    document.getElementById("prij").innerHTML = "Odjava";
+	document.getElementById("uname").innerHTML = "prijavljen";	
+     </script>';
+   }//od if 
+   else{
+	 	  echo $a0; 
+   }
+      echo '</ul></nav>';
+   }//od construct
+}//od class MenuBiznis  
+$adminAnestiz = new MenuBiznis(); 
+$uname = !empty($_SESSION["uname"]) ? $_SESSION["uname"] : "";
+
+
+
 ?>
