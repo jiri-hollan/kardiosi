ffc206340133288102d92d77b027874f28370105
Jiri Hollan <jiri.hollan@gmail.com>
2024-05-24 16:21:37 +0200

    začetek gradnje admin.php

--- kardio/admin/admin.php
+++ kardio/admin/admin.php
@@ -1,21 +1,8 @@
-<!DOCTYPE html>
+<?php
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 
+require_once('sabloni/vkladane/zahlavi.php');
+require_once('administrace.php');
 
-<html lang="sl-SI">
-<head>
-<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="shortcut icon" href="../favicon.ico?<?php echo time(); ?>">
-  <title>Anestezija za operacije srca</title>
-
-</head>
-
-
-      <body>
-        <p>fajl admin.php</p>
-      </body>
-    </noframes>
-  </frameset>
-</frameset>
-</html>
+?>
