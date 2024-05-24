ea35833290d23ad2d0a1dba2fe14f789f2b61972
Jiri Hollan <jiri.hollan@gmail.com>
2024-05-24 18:30:56 +0200

    esetsko popravilo kode

--- kardio/admin/admin.php
+++ kardio/admin/admin.php
@@ -5,29 +5,23 @@ header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 require_once('sabloni/vkladane/zahlavi.php');
 require_once('administrace.php');
 
-
-
-
-
 class MenuBiznis  {
    public function __construct() { 
  $a0= '
-
 ';
+
 $a1= '
-
 ';
+
 $a2= '
-
 '; 
-$a3= '
 
+$a3= '
 ';  
 echo '
 <nav id= "glavnaNav">
 <ul id="links">';
   if (isset($_SESSION["status"]))  {
-	  //require_once('../skupne/menu-items.php'); 
 	   switch ($_SESSION["status"]) {		   
 	case 1:
 	  
@@ -37,7 +31,6 @@ echo '
 	 break;	 
 	 case 3:
 	 require_once('../admin/databazeMenu.php');
-	 // header("Location: ../admin/databazeMenu.php");
 	   echo $a3;
     break;   
     default:
