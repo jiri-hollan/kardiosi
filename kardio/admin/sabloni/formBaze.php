e729e37f8819c687c518f22e87444dc9f7583449
Jiri Hollan <jiri.hollan@gmail.com>
2024-05-31 20:28:27 +0200

    nepomembno

--- kardio/admin/sabloni/formBaze.php
+++ kardio/admin/sabloni/formBaze.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_REQUEST["tabulka"])){
   $tab=$_REQUEST["tabulka"];
-// echo "Tabulka je: ".$tab;
+//echo "Tabulka je: ".$tab;
  echo strtoupper(rtrim($tab,"Tbl"));
  //echo md5("kvit");
   } 
