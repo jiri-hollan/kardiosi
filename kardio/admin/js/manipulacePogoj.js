b1710056e78a98e7475f61d23a665ee7e41bb270
Jiri Hollan <jiri.hollan@gmail.com>
2024-06-01 15:29:37 +0200

    estetsko urejena koda

--- kardio/admin/js/manipulacePogoj.js
+++ kardio/admin/js/manipulacePogoj.js
@@ -29,18 +29,19 @@ switch(akce) {
   case "edit":
        switch(tabulka) {
 		  case "statusiTbl":   
-//alert("v JS case edit");
-     if(document.getElementById("osebe")!=null){
-       document.getElementById("osebe").addEventListener("click", functionOverStatusi);
-}
-        break;
-	/*	  case "uporabnikiTbl": 
-		if(document.getElementById("osebe")!=null){
-     document.getElementById("osebe").addEventListener("click", functionOverUporabniki);
-}  
-       break;
-	   default:
-	   alert(nekaj je narobe);*/
+		   //alert("v JS case edit");
+			 if(document.getElementById("osebe")!=null){
+				document.getElementById("osebe").addEventListener("click", functionOverStatusi);
+			   }
+		  break;
+		/*case "uporabnikiTbl": 
+		   //alert("v JS case edit");		
+			if(document.getElementById("osebe")!=null){
+				document.getElementById("osebe").addEventListener("click", functionOverUporabniki);
+			   }  
+		   break;
+		   default:
+		   alert(nekaj je narobe);*/
 	   }
     break;
 
