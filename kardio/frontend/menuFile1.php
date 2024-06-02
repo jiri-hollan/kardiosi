b70d95e9dde1587e115defbe831ea91021a0db38
Jiri Hollan <jiri.hollan@gmail.com>
2024-06-02 20:53:18 +0200

    dodan var dump

--- kardio/frontend/menuFile1.php
+++ kardio/frontend/menuFile1.php
@@ -4,7 +4,8 @@ header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 
 require_once('sabloni/vkladane/zahlavi.php');
 require_once('administrace.php');
-
+echo "v frontend/menufile1<br>";
+      var_dump($_SESSION);	  
 $direktorij = "";
 $l="links.php";
 $p="";    
