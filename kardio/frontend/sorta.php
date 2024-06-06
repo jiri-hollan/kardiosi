f3ad59bc64faf83bd312018b078de8d5e3f64b7a
Jiri Hollan <jiri.hollan@gmail.com>
2024-06-06 20:33:20 +0200

    izbris zakomentiranega in praznih vrtic

--- kardio/frontend/sorta.php
+++ kardio/frontend/sorta.php
@@ -1,32 +1,8 @@
-<!--<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<style type="text/css">
-body {background-color: wheat}
-a {
-color: black
-}
-a {
-text-decoration: none
-}
-a.zdrav:hover {font-size: 130%;background-color:maroon; color:white}
-a.okro:hover {font-size: 130%;background-color:blue; color:yellow}
-
-
-</style>-->
 <?php
 require_once('sabloni/zahlaviSkupne.php');
 ?>
-
-
 </head>
 <body id="sorta">
-
 <script>
 var skupina = "ČLANI";
 var b ="";
