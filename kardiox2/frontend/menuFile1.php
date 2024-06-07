5064cfab06e06a267688952ccee90d987003aba8
Jiri Hollan <jiri.hollan@gmail.com>
2024-06-07 22:23:28 +0200

    zakomentiran header

--- kardiox2/frontend/menuFile1.php
+++ kardiox2/frontend/menuFile1.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-header("Cache-Control: no-cache, must-revalidate");
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+/*header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");*/
 require_once('sabloni/zahlaviSkupne.php');
 require_once('administrace.php');
 
