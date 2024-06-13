0d8c9f59306f6168ed8ee3351af6793ebb7e82b4
Jiri Hollan <jiri.hollan@gmail.com>
2024-06-13 10:43:47 +0200

    dodamo do menu biznis/servis

--- kardiox2/servis/servis.php
+++ kardiox2/servis/servis.php
@@ -7,7 +7,10 @@ echo'
     <ul class="right">
 	  <li class=""><a href="../navodila/formUploadBesedilo.php?direktorij=../besedila/"><h1>naloži besedilo</h1></a></li> 
 	  <li class=""><a href="upload/formUpload.php"><h1>naloži mesečni razpis PDF</h1></a></li>
-	  <li class=""><a href="manipulaceUporabniki.php?nazaj='.$nazaj.'">kompetence</a></li> 
+	  <li class=""><a href="manipulaceUporabniki.php?nazaj='.$nazaj.'">kompetence</a></li>
+	  <li class=""><a href="manipulacePregledovalci.php?nazaj='.$nazaj.'">pregledovalci</a></li>
+	  <li class=""><a href="manipulaceSklepi.php?nazaj='.$nazaj.'">prednastavljeni sklepi</a></li>
+	  <li class=""><a href="manipulaceLimiti.php?nazaj='.$nazaj.'">Mejne vrednosti</a></li>	  
     </ul>
  </section>
 ';
