40c8e2519380a4e38f01a4aec0a7a21d4aec261c
Jiri Hollan <jiri.hollan@gmail.com>
2024-06-16 13:34:02 +0200

    kopija programa pregled brez GIT

---
+++ pregled/skupne/menu-items.php
@@ -0,0 +1,6 @@
+<?php
+     $a0= '<li><span onclick=sbFunction("spomin")>Pregled</span>  </li>'; 
+	 $a1= '';
+	 $a2= '<li><a href="../servis/nastavitveMenu.php">nastavitve</a> </li>'; 
+	 $a3= '<li><a href="../admin/databaseMenu.php">database</a> </li>'; 
+?>
