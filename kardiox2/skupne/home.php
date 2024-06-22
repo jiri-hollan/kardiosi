f5e9f029c153734870f0e04aa470397cb58cda64
Jiri Hollan <jiri.hollan@gmail.com>
2024-06-22 22:57:32 +0200

    velke črke v male

--- kardiox2/skupne/home.php
+++ kardiox2/skupne/home.php
@@ -3,6 +3,6 @@ if (isset($_GET['doma'])) {
 	$doma=$_GET['doma'];
 	$home = '../' . $doma . '/menuFile1.php';
 } else {
-	$home = '../../kARDIOX2/frontend/menuFile1.php';
+	$home = '../../kardiox2/frontend/menuFile1.php';
 }
 ?>
