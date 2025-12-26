3d55eef849bd1787bfe6df262861d81cf10aaf35
Jiri Hollan <jiri.hollan@gmail.com>
2025-12-26 08:15:17 +0100

    izbris zaixanega

--- premedikacijaNavodilaX/premedikacija.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once ('../navodila/sabloni/vkladane/zahlavi.php');
-$nazaj="../navodila/menuNavodila.php"; 
-echo '<a id="buttonNazaj" href="' . $nazaj . '" >Nazaj</a>';
-require_once('sabloni/formaOtroskaPremedikacija.php');
-require_once ('otroskaPremedikacija1.php');
-require_once ('sabloni/prikaz.php');
-require_once('../navodila/sabloni/vkladane/zapati.php');
-?>
--- premedikacijaNavodilaX/sabloni/prikaz.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo'
-<br>uporabljeno zdravilo: <br>
-    <div id="navodila" class="prikaz"  name="navodila"  ></div>
-<br>aplikacija:<br>
-	 <div id="premedPredOp" class="prikaz"  name="premedPredOp" ></div>
-';
-?>
