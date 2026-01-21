1db0b44955013d91402f7ea30407c1ee134e8469
Jiri Hollan <jiri.hollan@gmail.com>
2026-01-21 17:42:41 +0100

    ena zamenjana

--- skupne/js/seznamBolnisnic.js
+++ skupne/js/seznamBolnisnic.js
@@ -2,8 +2,8 @@ function seznamBolnisnicFunction(bolList) {
 //alert ("seznamBolnisnicFunction");
 // koda, ki naredi array bolList iz tabele bolnisniceTab	
 //alert(bolList);
-var text = "";
-var i;
+let text = "";
+let i;
 for (i = 0; i < bolList.length; i++) {
   text += "<option value='" +  bolList[i]  + "'>"+ bolList[i] +"</option>";
 }
